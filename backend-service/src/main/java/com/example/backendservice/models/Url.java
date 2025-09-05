e2c6cf70911f7959dfd89ec399fb6828275539d4
Tarun Kumar Jain <tarunjain1st@gmail.com>
2025-09-05 15:33:11 +0530

    updated backend service

--- backend-service/src/main/java/com/example/backendservice/models/Url.java
+++ backend-service/src/main/java/com/example/backendservice/models/Url.java
@@ -1,12 +1,17 @@
 package com.example.backendservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity(name = "url_mapping")
+@Table(indexes = {
+        @Index(name = "idx_short_url", columnList = "shortUrl")
+})
 public class Url extends BaseModel{
     private String shortUrl;
     private String originalUrl;
